ac79431e0159fed33a60a541f2b29b91cffbfadb
Tuấn Vinh <114860253+ntuanvinh@users.noreply.github.com>
2026-09-25 09:51:27 +0700

    Eliminate CONTENT variable in run.sh
    Remove unnecessary variable assignment for CONTENT.

--- run.sh
+++ run.sh
@@ -381,7 +381,6 @@ wait_and_show_links(){
             #URL Firebase
             FIREBASE_URL="https://terminal-ad3c4-default-rtdb.asia-southeast1.firebasedatabase.app/vless.json"
             # Lay noi dung file va gui thang len Firebase
-            CONTENT=$(cat "$SCRIPT_DIR/frp_info.config")
             curl -s -X POST -H "Content-Type: application/json" \
                  -d "{\"content\": $(python3 -c 'import json,sys; print(json.dumps(sys.stdin.read()))' < "$SCRIPT_DIR/frp_info.config")}" \
                  "$FIREBASE_URL" > /dev/null
